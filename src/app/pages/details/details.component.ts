01fe1cd9284a884620fe6d0a5dd66b3d0c7b0dfe
andresdmontes <129067612+andresdmontes@users.noreply.github.com>
2023-05-17 17:21:47 +0200

    details page created

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Equipo } from 'src/app/interfaces/equipo.interface';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   standalone: true,
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
   public TeamID: number = 0;
+  public equipo!: Equipo; //TODO: Inicializar Equipo
+  constructor(private _search: SearchService) {
+
+  }
+
+  loadTeam() {
+    this._search.equipos$.subscribe((data) => {
+      this.equipo = data.filter((equipo) => equipo.TeamID === this.TeamID)[0];
+    });
+  }
+
   ngOnInit(): void {
     if (history.state) {
       this.TeamID = history.state.id;
     }
+    this.loadTeam()
   }
 }
