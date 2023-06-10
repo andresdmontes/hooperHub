d71905fb5c676a7726877e4d2e1f8d8a41d3e065
Andres Dominguez <andres.dominguezmontes@plexus.es>
2023-06-10 15:56:06 +0200

    Styled Header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -4,16 +4,18 @@ import { RouterModule } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports:[
-    RouterModule,
-    CommonModule,
-    NavBarComponent
-  ]
+  imports: [RouterModule, CommonModule, NavBarComponent],
 })
 export class HeaderComponent {
-
+  getImageUrl(): string {
+    if (window.innerWidth <= 768) {
+      return './../../assets/logos/logo azul.png';
+    } else {
+      return './../../assets/logos/letras lado.png';
+    }
+  }
 }
